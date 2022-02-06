ab0acef290dcd6f1d7cf0a0729b77393baab2d3b
Balaji <hackwithbalaji@gmail.com>
2022-02-06 22:29:47 +0530

    Added login api skeleton to check the credentials

--- app.py
+++ app.py
@@ -1,10 +1,36 @@
-import imp
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
+
+users = [
+    {
+        'uname' : 'balaji',
+        'password' : 123
+    }
+]
+
+def verify_user(data):
+    for user in users:
+        if user['uname'] == data['uname'] and user['password'] == data['password']:
+            return True
+    return False
 
 @app.route("/")
 def home():
     return "Home page"
+
+@app.route('/login', methods=['POST'])
+def login():
+    """
+        Method to login user
+    """
+    user_credentials = request.get_json()
+    if verify_user(user_credentials):
+        return jsonify({'message' : 'Login Success'})
+    
+    return jsonify({'message' : 'Login Failed'})
+
+    
+
 
 app.run(port = 5000)
